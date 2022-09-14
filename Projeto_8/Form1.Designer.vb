445e336a718a8a4e2b08fe033ea7a917b10a7193
DaniSanches <danielle.sanches@fatec.sp.gov.br>
2022-09-14 10:59:04 -0300

    versão inicial commit

--- Projeto_8/Form1.Designer.vb
+++ Projeto_8/Form1.Designer.vb
@@ -22,24 +22,76 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAluno = New System.Windows.Forms.Label()
+        Me.lblRgm = New System.Windows.Forms.Label()
+        Me.TxtRgm = New System.Windows.Forms.TextBox()
+        Me.lblNome = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnFechar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblAluno
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(86, 44)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(34, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Aluno"
+        Me.lblAluno.AutoSize = True
+        Me.lblAluno.Location = New System.Drawing.Point(86, 44)
+        Me.lblAluno.Name = "lblAluno"
+        Me.lblAluno.Size = New System.Drawing.Size(34, 13)
+        Me.lblAluno.TabIndex = 0
+        Me.lblAluno.Text = "Aluno"
+        '
+        'lblRgm
+        '
+        Me.lblRgm.AutoSize = True
+        Me.lblRgm.Location = New System.Drawing.Point(89, 82)
+        Me.lblRgm.Name = "lblRgm"
+        Me.lblRgm.Size = New System.Drawing.Size(38, 13)
+        Me.lblRgm.TabIndex = 1
+        Me.lblRgm.Text = "RGM: "
+        '
+        'TxtRgm
+        '
+        Me.TxtRgm.Location = New System.Drawing.Point(126, 79)
+        Me.TxtRgm.Name = "TxtRgm"
+        Me.TxtRgm.Size = New System.Drawing.Size(164, 20)
+        Me.TxtRgm.TabIndex = 2
+        '
+        'lblNome
+        '
+        Me.lblNome.AutoSize = True
+        Me.lblNome.Location = New System.Drawing.Point(85, 127)
+        Me.lblNome.Name = "lblNome"
+        Me.lblNome.Size = New System.Drawing.Size(35, 13)
+        Me.lblNome.TabIndex = 3
+        Me.lblNome.Text = "Nome"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(126, 124)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(260, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'btnFechar
+        '
+        Me.btnFechar.Location = New System.Drawing.Point(311, 79)
+        Me.btnFechar.Name = "btnFechar"
+        Me.btnFechar.Size = New System.Drawing.Size(75, 23)
+        Me.btnFechar.TabIndex = 5
+        Me.btnFechar.Text = "Fechar"
+        Me.btnFechar.UseVisualStyleBackColor = True
+        Me.btnFechar.UseWaitCursor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnFechar)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblNome)
+        Me.Controls.Add(Me.TxtRgm)
+        Me.Controls.Add(Me.lblRgm)
+        Me.Controls.Add(Me.lblAluno)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -47,5 +99,10 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblAluno As Label
+    Friend WithEvents lblRgm As Label
+    Friend WithEvents TxtRgm As TextBox
+    Friend WithEvents lblNome As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnFechar As Button
 End Class
---
+++ Projeto_8/Pessoa.vb
@@ -0,0 +1,16 @@
+﻿Public Class Pessoa
+    ' Private  para  que ele não possa ser acessado por uma classe que não seja dele
+    Private nome As String
+
+    Public Property PNome As String
+
+    Public Property PNome As String
+        Get
+            Return nome
+        End Get
+        Set(value As String)
+            nome = value
+        End Set
+    End Property
+
+End Class
